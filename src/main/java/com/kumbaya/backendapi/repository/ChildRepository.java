1ec8019dd519efbaabb10cb586802a93f96e2e91
Michel Zuidema <michelgzuidema@gmail.com>
2021-05-31 14:54:00 +0200

    Get Child By Parent Functionality.

--- src/main/java/com/kumbaya/backendapi/repository/ChildRepository.java
+++ src/main/java/com/kumbaya/backendapi/repository/ChildRepository.java
@@ -1,7 +1,11 @@
 package com.kumbaya.backendapi.repository;
 
 import com.kumbaya.backendapi.entity.Child;
+import com.kumbaya.backendapi.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ChildRepository extends CrudRepository<Child, Integer> {
+    List<Child> findByParent(User user);
 }
--- src/main/java/com/kumbaya/backendapi/service/UserService.java
+++ src/main/java/com/kumbaya/backendapi/service/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -27,5 +29,16 @@ public class UserService {
         } else {
             return new ApiResponse(false, "The given email was not valid, please try again.");
         }
+    }
+
+    /**
+     * This method checks if a user exists with the specific id
+     * @param userId Unique ID of the user who has to be checked
+     * @return Boolean If user exists
+     */
+    public Boolean doesUserExist(Integer userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        return userOptional.isPresent();
     }
 }
