4a63c818d3cd0e0b714ccfef24aa4720bc39b297
Michel Zuidema <michelgzuidema@gmail.com>
2021-06-15 14:21:34 +0200

    README

--- src/main/java/com/kumbaya/backendapi/controller/ItemController.java
+++ src/main/java/com/kumbaya/backendapi/controller/ItemController.java
@@ -1,13 +1,18 @@
 package com.kumbaya.backendapi.controller;
 
+import com.kumbaya.backendapi.entity.Category;
 import com.kumbaya.backendapi.entity.Item;
 import com.kumbaya.backendapi.model.response.ApiResponse;
+import com.kumbaya.backendapi.repository.CategoryRepository;
 import com.kumbaya.backendapi.repository.ItemRepository;
+import com.kumbaya.backendapi.service.CategoryService;
 import com.kumbaya.backendapi.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/api/item")
@@ -16,7 +21,13 @@ public class ItemController {
     private ItemRepository itemRepository;
 
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private ItemService itemService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping(path = "/{id}")
     public @ResponseBody
@@ -30,11 +41,26 @@ public class ItemController {
         return ResponseEntity.ok(new ApiResponse(false, "Item not found!"));
     }
 
+    @GetMapping(path = "/{categoryId}/items")
+    public @ResponseBody
+    ResponseEntity getItemsByCategory(@PathVariable Integer categoryId) {
+        if(categoryService.doesCategoryExist(categoryId)) {
+            Category category = categoryRepository.findById(categoryId).get();
+
+            List<Item> items = itemRepository.findByCategory(category);
+
+            return ResponseEntity.ok(new ApiResponse(true, items));
+        }
+
+        return ResponseEntity.ok(new ApiResponse(false, "Category not found!"));
+    }
+
     @PostMapping(path = "/add")
-    public @ResponseBody ResponseEntity addItem(@RequestBody Item item){
+    public @ResponseBody
+    ResponseEntity addItem(@RequestBody Item item) {
         ApiResponse validateItem = itemService.validateItem(item);
 
-        if(!validateItem.getSuccess()) {
+        if (!validateItem.getSuccess()) {
             return ResponseEntity.ok(new ApiResponse(false, validateItem.getMessage()));
         }
 
--- src/main/java/com/kumbaya/backendapi/repository/ItemRepository.java
+++ src/main/java/com/kumbaya/backendapi/repository/ItemRepository.java
@@ -1,7 +1,11 @@
 package com.kumbaya.backendapi.repository;
 
+import com.kumbaya.backendapi.entity.Category;
 import com.kumbaya.backendapi.entity.Item;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends CrudRepository<Item, Integer> {
+    List<Item> findByCategory(Category category);
 }
